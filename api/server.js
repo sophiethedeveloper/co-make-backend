ace61d1b6d83947c08d1e3aef4bde3f9e0786356
sofia Luna <Sofia21luna@gmail.com>
2020-11-15 17:20:01 -0600

    auth and registration

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const authRouter = require("../auth/auth-router.js");
 
 const server = express();
 server.use(helmet());
@@ -12,5 +13,7 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.json({ message: "api is running" });
 });
+
+server.use("/api/auth", authRouter);
 
 module.exports = server;
