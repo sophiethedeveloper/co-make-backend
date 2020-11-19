463a1a9864a2851d4e7b2c2e1c4e5017f785866b
sofia Luna <Sofia21luna@gmail.com>
2020-11-19 14:12:39 -0600

    testing for models

--- models/auth-models.test.js
+++ models/auth-models.test.js
@@ -11,15 +11,42 @@ describe("Authorization", () => {
       const users = await Auth.find();
       expect(users).toHaveLength(0);
     });
+
+    it("gets all the users", async () => {
+      await db("users").insert({
+        email: "sofialu@gmail.com",
+        password: "12345",
+      });
+      let users = await Auth.find();
+      expect(users).toHaveLength(1);
+
+      await db("users").insert({
+        email: "briank@gmail.com",
+        password: "12345",
+      });
+      users = await Auth.find();
+      expect(users).toHaveLength(2);
+    });
   });
 
-  it("gets all the users", async () => {
-    await db("users").insert({ email: "sofialu@gmail.com", password: "12345" });
-    let users = await Auth.find();
-    expect(users).toHaveLength(1);
+  describe("add method", () => {
+    it("can insert new cars", async () => {
+      await Auth.add({ email: "sofialu@gmail.com", password: "12345" });
+      let users = await Auth.find();
+      expect(users).toHaveLength(1);
 
-    await db("users").insert({ email: "briank@gmail.com", password: "12345" });
-    users = await Auth.find();
-    expect(users).toHaveLength(2);
+      await Auth.add({ email: "briank@gmail.com", password: "12345" });
+      users = await Auth.find();
+      expect(users).toHaveLength(2);
+    });
+  });
+
+  describe("findById method", () => {
+    it("finds by id", async () => {
+      await db("users").insert({ email: "sofia@gmail.com", password: "12345" });
+      let userObject = await Auth.findById(1);
+      expect(userObject.email).toBe("sofia@gmail.com");
+      expect(userObject.password).toBe("12345");
+    });
   });
 });
